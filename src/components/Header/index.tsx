d631d1e6a7e1cb1973490dbf81989630cffa32e2
GabrielWojcik <gabrielwojcikv@gmail.com>
2025-01-14 21:20:46 -0300

    feat - new desgin

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,19 +1,19 @@
 import Image from "next/image";
-import Me from "../../assets/icons/Me.svg";
+import Me2 from "../../assets/icons/Me2.png";
 import Gradient from "../../assets/icons/Gradient.svg";
 import Arrow from "../../assets/icons/Arrow.svg";
 import Elipse from "../../assets/icons/Ellipse.svg";
 
 export function Header() {
     return(
-        <div className="w-4/6 pt-16">
-            <div className=" flex  justify-end w-[80%]">
+        <div className="flex flex-col items-center  pt-16">
+            <div className=" flex justify-center items-center w-[100%]">
                 <Image src={Arrow} alt="" />
                 <p className="relative top-6 text-[#7127BA]">Gabriel Wojcik</p>
             </div>
             <div className="flex justify-end items-center">
                 <div className="relative">
-                    <Image src={Me} alt="" />
+                    <Image src={Me2} alt="" width={350} height={350}/>
                 <div className="absolute top-16">
                     <Image src={Gradient} alt="" />
                 </div>
@@ -28,20 +28,29 @@ export function Header() {
                     <Image src={Elipse} alt="" />
                 </div>
             </div>
-            <div className="flex flex-col justify-end items-end pt-10">
+            <div className="flex flex-col justify-end items-end ml-10 pt-10">
                 <div className="flex flex-col justify-end items-center text-white">
                     <p className="text-5xl">
                         Desenvolvedor Front-end 
                     </p>
+                    <p className="text-xl">
+                        Sou um desenvolvedor front-end autodidata com 4 anos de experiência, 
+                        especializado em ReactJS e NextJS. Crio soluções digitais que equilibram 
+                        as necessidades dos usuários com os objetivos de negócios, sempre focando em 
+                        desempenho e escalabilidade. 
+                        Tenho certificação AWS Cloud Practitioner, o que me permite integrar minhas 
+                        habilidades de desenvolvimento com a nuvem para criar produtos eficientes e de 
+                        alta qualidade.
+                    </p>
                 </div>
 
-                <div className="flex flex-col w-[57%] justify-end items-center text-white mt-6">
+                {/* <div className="flex flex-col w-[57%] justify-end items-center text-white mt-6">
                     <p className="text-xl">
                         A self-taught UI/UX designer, functioning in the industry for 3+ years now.
                         I make meaningful and delightful digital products that create an equilibrium
                         between user needs and business goals.
                     </p>
-                </div>
+                </div> */}
             </div>
 
 
--- src/components/Work/index.tsx
+++ src/components/Work/index.tsx
@@ -1,23 +1,67 @@
 import Image from "next/image";
+import Icon1 from "../../assets/icons/Rectangle.svg";
+import Icon2 from "../../assets/icons/Rectangle2.svg";
+import Icon3 from "../../assets/icons/Rectangle3.svg";
+import Icon4 from "../../assets/icons/Rectangle4.svg";
 import Gradient from "../../assets/icons/Big-Gradient.svg";
 
 
 export function WorkExpirience() {
     return(
-        <div className="flex flex-col justify-center items-center text-white">
-            <p className="text-4xl m-5">
+        <div className="flex flex-col justify-center items-center pt-10 text-white">
+            <p className="text-4xl m-5 justify-start">
                 Experiência Profissional
             </p>
 
-            <div className="relative">
-                <div>
-                    <div>
-                        <p className="text-white">cardss</p>
-
+            <div className="flex flex-wrap items-center justify-center w-1/2 ">
+                <div className="flex flex-wrap justify-center  gap-4">
+                    <div className="flex border-t-4 border-[#693B93] w-80 bg-[#4F228D] rounded-lg p-2">
+                        <Image src={Icon1} alt="" />
+                        <div>
+                            <p className="text-white text-[26px]">SO PETS</p>
+                            <p className="text-[8px]">App desenvolvido em JavaScript e React Native com foco no registro de exames/vacinações de animais de estimação e agendamento de consultas médicas</p>
+                            <button 
+                            className="bg-[#2C1250] border-2 border-[#693B93] rounded-lg p-2 text-[10px] mt-2">
+                                Saiba Mais
+                            </button>
+                        </div>
+                    </div>
+                    <div className="flex border-t-4 border-[#693B93] w-80 bg-[#4F228D] rounded-lg p-2">
+                        <Image src={Icon2} alt="" />
+                        <div>
+                            <p className="text-white text-[26px]">Ecommerce-card</p>
+                            <p className="text-[8px]">Projeto dedicado à construção de um e-commerce com o objetivo de testar novas tecnologias e métodos de código.</p>
+                            <button 
+                            className="bg-[#2C1250] border-2 border-[#693B93] rounded-lg p-2 text-[10px] mt-2">
+                                Saiba Mais
+                            </button>
+                        </div>
+                    </div>
+                    <div className="flex border-t-4 border-[#693B93] w-80 bg-[#4F228D] rounded-lg p-2">
+                        <Image src={Icon3} alt="" />
+                        <div>
+                            <p className="text-white text-[26px]">Car hub</p>
+                            <p className="text-[8px]">Este projeto tem como objetivo oferecer uma plataforma online onde os usuários podem reservar carros de forma prática e segura. Foi desenvolvido com tecnologias modernas para garantir uma experiênc…</p>
+                            <button 
+                            className="bg-[#2C1250] border-2 border-[#693B93] rounded-lg p-2 text-[10px] mt-2">
+                                Saiba Mais
+                            </button>
+                        </div>
+                    </div>
+                    <div className="flex border-t-4 border-[#693B93] w-80 bg-[#4F228D] rounded-lg p-2">
+                        <Image src={Icon4} alt="" />
+                        <div>
+                            <p className="text-white text-[26px]">Covid-API</p>
+                            <p className="text-[8px]">Script para exibir a média diária da Covid-19.</p>
+                            <button 
+                            className="bg-[#2C1250] border-2 border-[#693B93] rounded-lg p-2 text-[10px] mt-2">
+                                Saiba Mais
+                            </button>
+                        </div>
                     </div>
                 </div>
 
-                <div>
+                <div className="">
                     <Image src={Gradient} alt="" />
                 </div>
 
