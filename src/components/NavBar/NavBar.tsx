57b34af2887743f686178e3f6c8aeab6c7e75ac9
Gabriel Wojcik | VEGA I.T. <gabrielk@vegait.com>
2025-01-15 12:04:35 -0300

    fix

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -2,15 +2,7 @@ import Image from "next/image";
 import Logo from "../../assets/icons/Logo.png";
 
 export function NavBar() {
-    const scrollToHeader = () => {
-        const headerElement = document.getElementById("header");
-        if (headerElement) {
-            headerElement.scrollIntoView({
-                behavior: "smooth",
-                block: "start",
-            });
-        }
-    };
+   
 
     return (
         <div className="flex px-4 md:px-16 items-center bg-[#1A0B2E] w-[100%] h-28 fixed top-0 left-0 z-10">
@@ -21,7 +13,7 @@ export function NavBar() {
             <div className="w-6/12">
                 <ul className="flex justify-around text-white">
                     <li className="cursor-pointer">Home</li>
-                    <li className="cursor-pointer" onClick={scrollToHeader}>Sobre</li>
+                    <li className="cursor-pointer">Sobre</li>
                     <li className="cursor-pointer">Contato</li>
                 </ul>
             </div>
